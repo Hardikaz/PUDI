1ba35ca0639740ecb4377f1f34394359ade523d4
Hardik Sharma <sharmahardikaz@gmail.com>
2023-10-20 18:42:34 +0530

    Made the BG section

--- pudi-app/src/Components/section-bg.jsx
+++ pudi-app/src/Components/section-bg.jsx
@@ -1,13 +1,24 @@
 import React from "react"
-
+import BG1 from "./Assets/bg1.png"
+import BG2 from "./Assets/bg2.gif"
+import BG3 from "./Assets/bg3.png"
 const SectionBg = () => {
 return (
+  <div> 
     <div className="sectionBg">
-        <img decoding="async" className="img-slide active" src="/Assets/bg1.png" alt="Here is an image"></img>
-        <img decoding="async" className="img-slide" src="/Assets/bg2.gif" alt="Here is an image"></img>
-        <img decoding="async" className="img-slide" src="/Assets/bg3.png" alt="Here is an image"></img>
-       
+        <img decoding="async" className="img-slide active" src={BG1} alt="Here is an image 1"></img>
+        {/* <img decoding="async" className="img-slide" src={BG2} alt="Here is an image 2"></img>
+        <img className="img-slide" src={BG3} alt="Here is an image 3"/>
+        */}
     </div>
+    <div class="slider-navigation">
+            <div class="nav-btn active"></div>
+            <div class="nav-btn"></div>
+            <div class="nav-btn"></div>
+            <div class="nav-btn"></div>
+            <div class="nav-btn"></div>
+    </div>
+</div> 
 )
 }
 
