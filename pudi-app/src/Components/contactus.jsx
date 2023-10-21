a6d3918cd645b7fb52a82562af722980ee1b0816
Hardik Sharma <sharmahardikaz@gmail.com>
2023-10-21 23:02:51 +0530

    Contact Us created

--- pudi-app/src/Components/contactus.jsx
+++ pudi-app/src/Components/contactus.jsx
@@ -9,11 +9,23 @@ const contactUs = () => {
 
         <div className="contactUs-body hidden">
         <ul>
-            <li><img src={Call} alt="icon here"/><span>+91-7535900771</span></li>
-            <li><img src={Email} alt="icon here"/><span>www.pudi.co.in</span></li>
-            <li><img src={Web} alt="icon here"/><span>info@pudi.co.in</span></li>
-            <li><textarea placeholder="Write a message"></textarea></li>
+            <li><img src={Call} alt="icon here"/><span  className="contactUs-info">+91-7535900771</span></li>
+            <li><img src={Email} style={{height:"60px", borderRadius:"10px"}} alt="icon here"/><span className="contactUs-info">www.pudi.co.in</span></li>
+            <li><img src={Web} style={{height:"80px"}}alt="icon here"/><span className="contactUs-info">info@pudi.co.in</span></li>
+            {/* <li><textarea placeholder="Write a message"></textarea></li> */}
         </ul>
+        <div className="divider"></div>
+        <div className="contactUs-body-form">
+            <form action="#" method="POST">
+            <label for="name">Name: </label> 
+            <input type="text" className="input" placeholder="Enter your name" name="name"/>
+            <label for="number">Number: </label> 
+            <input type="number" className="input" placeholder="Enter your name" name="number"/>
+            <label for="area">Your Message: </label> 
+            <textarea placeholder="Write a message" name="area" className="input-area" ></textarea>
+            <button type="submit">Submit</button>
+            </form>
+        </div>
         </div>
         </div>
     )
