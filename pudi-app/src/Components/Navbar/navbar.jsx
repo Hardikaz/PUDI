b2cc71b1d112cff46d10d2332a2de0659ca98023
Hardik Sharma <sharmahardikaz@gmail.com>
2023-11-08 16:10:46 +0530

    Our Team button added

--- pudi-app/src/Components/Navbar/navbar.jsx
+++ pudi-app/src/Components/Navbar/navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import logo from "../Assets/logo.png"
-
+import Button from 'react-bootstrap/Button';
 
 const Navbar = () => {
     return (
@@ -13,8 +13,9 @@ const Navbar = () => {
                    <a href="#about" style={{textDecoration:"none"}}><li>About PUDI</li></a>
                    <a href="#feature" style={{textDecoration:"none"}}><li>Features</li></a>
                    <a href="#contact" style={{textDecoration:"none"}}><li>Contact US</li></a>
-
+                   <a ><Button variant="outline-success">Our Team</Button>{' '}</a>
                 </ul>
+                
             </div>
         </div>
     )
