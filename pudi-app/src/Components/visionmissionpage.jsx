551805f12d35158a360367cd03c9afe9a63567b0
Hardik Sharma <sharmahardikaz@gmail.com>
2023-12-15 14:30:36 +0530

    Our Team Route completed

--- pudi-app/src/Components/visionmissionpage.jsx
+++ pudi-app/src/Components/visionmissionpage.jsx
@@ -2,7 +2,20 @@
 const vimi = () => {
     return (
         <>
-        <div>imi page</div>
+        <div className="vimi">
+        <div className="vision-card">
+        <div className="vision-card-heading">
+            VISION
+        </div>
+        <p>Our vision is to offer packaging excellence characterized by excellent quality, unrivaled service, continuous improvement, and ongoing innovation in order to become one of the top flexible packaging companies in the market.</p>
+        </div>
+        
+
+        <div className="mission-card">
+        <div className="mission-card-heading">MISSION</div>
+        <p>Our mission is to live out our core principles, which put the development and empowerment of our team members at the forefront. As a consequence, our staff will establish cherished, reliable, and long-lasting connections with customers and business partners.</p>
+        </div>
+        </div>
         </>
     )
 }
