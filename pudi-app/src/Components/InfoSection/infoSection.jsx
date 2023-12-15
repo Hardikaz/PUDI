4979039460175ea59fa74705dafdae120c7a7f13
Hardik Sharma <sharmahardikaz@gmail.com>
2023-12-15 22:53:02 +0530

    Editing

--- pudi-app/src/Components/InfoSection/infoSection.jsx
+++ pudi-app/src/Components/InfoSection/infoSection.jsx
@@ -17,9 +17,9 @@ const infoSection = (props) =>{
          <div className="infoSection-content-heading">
             <p data-aos="fade-up">PUDI implements sustainable packaging </p>
          </div>
-         <p data-aos="fade-up">The Environmental Protection Agency (EPA) estimates that <span style={{fontWeight:"bold",color:"rgb(3, 251, 152)",fontSize:"25px",fontStyle:"italic"}}>product packaging makes up 29.9% of the material</span> in municipal solid waste landfills, or 80.1 million tons of trash.
+         <p data-aos="fade-up" style={{fontWeight:"300"}}>The Environmental Protection Agency (EPA) estimates that <span style={{fontWeight:"bold",color:"rgb(3, 251, 152)",fontSize:"25px",fontStyle:"italic"}}>product packaging makes up 29.9% of the material</span> in municipal solid waste landfills, or 80.1 million tons of trash.
          <br/>
-         <br/>
+         
          <br/>
          Companies today need to be more conscious of the environmental impact of their packaging. The future lies in <span style={{fontWeight:"bold",color:"rgb(3, 251, 152)",fontSize:"25px",fontStyle:"italic"}}>sustainable packaging that consumes fewer resources, generates less trash, and promotes recycling.</span></p>
          </div>
--- pudi-app/src/Components/InfoSection/infoSection2.jsx
+++ pudi-app/src/Components/InfoSection/infoSection2.jsx
@@ -8,10 +8,10 @@ const infoSection2 = () =>{
                 <div className="infoSection2-content-heading">
                     <p data-aos="fade-up">Freshness is Vital</p>
                 </div>
-                <p data-aos="fade-up">Freshness is vital for the agriculture industry. Though some fruits remain safe to eat for months, <span style={{fontWeight:"bold",color:"rgb(3, 251, 152)",fontSize:"25px",fontStyle:"italic"}}>they may have shorter shelf lives for optimal flavor and freshness.</span>
+                <p data-aos="fade-up" style={{fontWeight:"300"}}>Freshness is vital for the agriculture industry. Though some fruits remain safe to eat for months, <span style={{fontWeight:"bold",color:"rgb(3, 251, 152)",fontSize:"25px",fontStyle:"italic"}}>they may have shorter shelf lives for optimal flavor and freshness.</span>
 ​                <br/>
 <br/>
-Freshness is a critical aspect when it comes to fruits. Fresh fruits not only taste better but are also more nutritious.
+<span style={{fontWeight:"300"}}>Freshness is a critical aspect when it comes to fruits. Fresh fruits not only taste better but are also more nutritious.</span>
 ​
 <span style={{fontWeight:"bold",color:"rgb(3, 251, 152)",fontSize:"25px",fontStyle:"italic"}}>Packaging must account for keeping the goods at their peak until purchase.</span></p>
                 </div>
